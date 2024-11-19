8a434f44a5437630ba57574b263b344e7512071b
lucasoneves <casluhc@gmail.com>
2024-11-18 21:20:28 -0300

    Layout de eventos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
 
   app: {
     head: {
       title: "Festou - Organize sua festa ou evento com facilidade",
       meta: [
-        { name: 'description', content: 'Organizador de festas e eventos'}
+        { name: "description", content: "Organizador de festas e eventos" },
       ],
-      link: [
-        { rel: 'stylesheet', href: ''}
-      ]
-    }
+      link: [{ rel: "stylesheet", href: "" }],
+    },
   },
 
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   postcss: {
     plugins: {
@@ -27,8 +25,8 @@ export default defineNuxtConfig({
   ssr: true,
 
   runtimeConfig: {
-    currencyKey: process.env.CURRENCY_API_KEY
+    currencyKey: process.env.CURRENCY_API_KEY,
   },
 
-  modules: ['@nuxt/image']
-})
+  modules: ["@nuxt/image"],
+});
--- pages/events/[_id]/guests/index.vue
+++ pages/events/[_id]/guests/index.vue
@@ -5,17 +5,17 @@
     <h3>Presença confirmada: 3</h3>
     <div
       v-for="user in users"
-      class="mt-6 flex gap-20 justify-center items-center shadow-sm mb-4 bg-[var(--clear)] px-4 py-4 rounded-lg"
+      class="mt-6 flex gap-4 md:gap-20 md:flex-row flex-col justify-center items-center shadow-sm mb-4 bg-[var(--clear)] px-4 py-4 rounded-lg"
     >
-      <div class="flex-1 flex justify-center flex-col items-start">
-        <div class="flex items-center gap-2">
+      <div class="flex justify-center flex-col items-center md:items-start w-full">
+        <div class="flex flex-col md:flex-row items-center gap-2">
           <NuxtImg
             :src="user.avatar"
             width="40"
             height="40"
             class="bordered border-2 rounded-full bg-slate-200"
           />
-          <div>
+          <div class="flex flex-col items-center md:items-start w-full">
             <p class="text-md font-bold">{{ user.name }}</p>
             <span class="text-xs">{{
               user.status % 2 ? "Confirmada" : "Pendente"
@@ -23,19 +23,20 @@
           </div>
         </div>
       </div>
-      <div class="flex-1 flex items-end gap-4">
+      <div class="w-full flex items-center justify-center md:items-end gap-4">
         <p class="text-xs">
           {{ user.payment ? "Pago" : "Pagamento pendente" }}
         </p>
       </div>
-      <div class="flex gap-4">
+      <div class="flex flex-wrap w-full justify-center md:justify-end gap-2 md:gap-4">
         <button
-          class="text-sm bg-[var(--france)] px-5 py-2 rounded-full text-white"
+          v-if="user.status !== 1"
+          class="w-full md:w-auto text-sm bg-[var(--france)] px-4 py-2 sm:px-5 rounded-full text-white"
         >
           Reenviar convite
         </button>
         <button
-          class="text-sm bg-[var(--amour-light)] px-5 py-2 rounded-full text-[var(--amour)]"
+          class="text-sm md:w-auto w-full px-5 py-2 rounded-full text-[var(--amour)]"
         >
           Excluir
         </button>
